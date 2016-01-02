a02244cd28ebb2e62ed33011b877a3386f53db34
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2016-01-02 13:28:05 +0100

    Add build and nuspec file

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" ProgParty.Core.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
+
+%nuget% pack "ProgParty.Core.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
